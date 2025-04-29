a1e15fd0a48be0efb2e33ea529aabb7743b0e7ab
pataibalazs <79509500+pataibalazs@users.noreply.github.com>
2025-04-29 20:46:33 +0200

    push

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.4000days.com" }],
+        destination: "https://4000days.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
